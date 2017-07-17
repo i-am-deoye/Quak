a166b67150123c064fb1c1f489c3af8e46aa8013
moses <mosestrendlr@gmail.com>
2017-07-17 14:33:13 +0100

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quak
+//
+//  Created by Moses on 17/07/2017.
+//  Copyright © 2017 Flint. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
